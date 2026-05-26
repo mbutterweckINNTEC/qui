a0160841e74484cb456a8b7b595a8273e9d09bdb
Michal Butterweck <michal@inntec.pl>
2026-05-26 12:52:25 +0200

    qui_mtrx: adding gui scale mtrix.

--- qui_mtrx.h
+++ qui_mtrx.h
@@ -4,6 +4,7 @@
 enum {
 	QUI_MTRX_P,	/* projection matrix */
 	QUI_MTRX_V,	/* view matrix */
+	QUI_MTRX_S,	/* gui scale matrix */
 
 	QUI_MTRX_N
 };
@@ -14,14 +15,14 @@ float44_t qui_mtrx_top(int typ);
 
 #ifdef QUI_IMPL
 
-#define QUI_MTRX_S 8
+#define QUI_MTRX_SZ 8
 
-float44_t qui_mtrx[QUI_MTRX_N][QUI_MTRX_S];
+float44_t qui_mtrx[QUI_MTRX_N][QUI_MTRX_SZ];
 int qui_mtrx_s[QUI_MTRX_N];
 
 int qui_mtrx_psh(int typ, float44_t M) {
 	if (typ < QUI_MTRX_N) {
-		if (qui_mtrx_s[typ] < QUI_MTRX_S) {
+		if (qui_mtrx_s[typ] < QUI_MTRX_SZ) {
 			qui_mtrx[typ][qui_mtrx_s[typ]++] = M;
 			return 0;
 		}
